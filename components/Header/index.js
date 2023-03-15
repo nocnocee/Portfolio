174400da5c9ac4adf482fac36b5e42d478c9593b
nocnoc_e <dhgustmd25@hotmail.com>
2023-03-15 15:35:21 -0400

    my portfolio

--- components/Header/index.js
+++ components/Header/index.js
@@ -85,7 +85,7 @@ const Header = ({ handleWorkScroll, handleAboutScroll, isBlog }) => {
                   )}
 
                   <Button
-                    onClick={() => window.open("mailto:hello@chetanverma.com")}
+                    onClick={() => window.open("mailto:enoch.oh@outlook.com")}
                   >
                     Contact
                   </Button>
